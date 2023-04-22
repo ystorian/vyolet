fb981798de4f104251c19a8539793744c014140a
Flavien Scheurer <flavien@ystorian.com>
2023-04-22 18:40:48 +0200

    Add 2 functions to convert UTC times to local time or date
    Signed-off-by: Flavien Scheurer <flavien@ystorian.com>

--- static/js/vyolet.js
+++ static/js/vyolet.js
@@ -29,3 +29,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 });
+
+// Convert UTC time to local time.
+// This works only on elements with a "utc-time" class.
+// Example:
+// <span class="utc-time">2023-04-22 18:09</span>
+// will be changed to 22.04.2023 20:09
+// Depending on browser support, the middle "T" and the seconds can be omitted.
+// The "Z" at the end, which indicates UTC time, is added automatically.
+document.addEventListener('DOMContentLoaded', () => {
+	(document.querySelectorAll('.utc-time') || []).forEach(($utcTime) => {
+		if($utcTime.textContent.indexOf('Z') === -1) $utcTime.textContent += 'Z';
+		const time = new Date($utcTime.textContent);
+		$utcTime.textContent = time.toLocaleString();
+	});
+});
+
+// Convert UTC date to local date.
+// This works only on elements with a "utc-date" class.
+// Example:
+// <span class="utc-date">2023-04-22 18:09</span>
+// will be changed to 22.04.2023
+// Depending on browser support, the middle "T" and the seconds can be omitted.
+// The "Z" at the end, which indicates UTC time, is added automatically.
+document.addEventListener('DOMContentLoaded', () => {
+	(document.querySelectorAll('.utc-date') || []).forEach(($utcDate) => {
+		if($utcDate.textContent.indexOf('Z') === -1) $utcDate.textContent += 'Z';
+		const time = new Date($utcDate.textContent);
+		$utcDate.textContent = time.toLocaleDateString();
+	});
+});
